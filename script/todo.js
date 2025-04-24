49e4bec8b461e43972a73b3ba6090650f6f79129
Gleb Vasilyev <glebvas.vrn@gmail.com>
2025-04-25 00:06:09 +0300

    second

--- script/todo.js
+++ script/todo.js
@@ -1,46 +1,30 @@
-const btnDone = document.querySelector('#btn-done');
-const btnDelete = document.querySelector('#btn-delete');
-let isBtnVisible = false;
-let isBtnDelVisible = false;
-
-btnDone.addEventListener('click', () => {
-    isBtnVisible = !isBtnVisible;
-
-    if (isBtnVisible) {
-        btnDone.classList.add('active');
-    } else {
-        btnDone.classList.remove('active');
-    }
-})
-btnDelete.addEventListener('click', () => {
-    isBtnDelVisible = !isBtnDelVisible;
-
-    if (isBtnDelVisible) {
-        btnDelete.classList.add('active');
-    } else {
-        btnDelete.classList.remove('active');
-    }
-})
-
-
-
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#input');
 const taskList = document.querySelector('#task-list');
+const emptyList = document.querySelector('#empty-list');
+const linesContainer = document.querySelector('#empty-lines');
 
-form.addEventListener('submit', function(event) {
+
+form.addEventListener('submit', addTask);
+form.addEventListener('reset', deleteDoneTasks)
+
+taskList.addEventListener('click', deleteTask);
+taskList.addEventListener('click', doneTask);
+
+function addTask(event) {
     event.preventDefault();
 
-    const taskText = taskInput.value;
+    const tasksCounter = taskList.children.length;
+    const taskText = tasksCounter + '. ' + taskInput.value;
 
     const taskHtml = `
                 <li class="list-group-item">
                     <span class="task-title">${taskText}</span>
                     <div class="group-item-button">
-                        <button type="button" data-action="done" id="btn-done" class="btn-action done">
+                        <button type="button" data-action="done" class="btn-action done">
 							<img src="icons/done.svg" alt="Done">
 						</button>
-						<button type="button" data-action="delete" id="btn-delete" class="btn-action delete">
+						<button type="button" data-action="delete" class="btn-action delete">
 							<img src="icons/delete.svg" alt="Done">
 						</button>
                     </div>
@@ -49,4 +33,77 @@ form.addEventListener('submit', function(event) {
 
     taskInput.value = '';
     
-});
+    if (tasksCounter >= 1) {
+        emptyList.classList.add('none');
+    }
+
+    if (tasksCounter <=6 && tasksCounter > 1) {
+        linesContainer.children[tasksCounter - 2].classList.add('none');
+    }
+};
+
+function deleteTask(event) {
+
+    if (event.target.dataset.action !== 'delete') {
+        return
+    }
+
+    const parentNode = event.target.closest('.list-group-item');
+    parentNode.remove();
+        
+    const tasksCounter = taskList.children.length;
+
+    if (tasksCounter === 1) {
+        emptyList.classList.remove('none');
+    }
+
+    if (tasksCounter <=6 && tasksCounter > 1) {
+        linesContainer.children[tasksCounter - 2].classList.remove('none');
+    }
+}
+
+function doneTask(event) {
+
+    if (event.target.dataset.action !== 'done') {
+        return
+    }
+
+    const btnDone = event.target.closest('.done');
+    btnDone.classList.toggle('active');
+
+    const parentNode = event.target.closest('.list-group-item');
+    const taskTitle = parentNode.querySelector('.task-title');
+    taskTitle.classList.toggle('task-title--done');
+}
+
+function deleteDoneTasks(event) {
+    event.preventDefault();
+    
+    const tasksCounter = taskList.children.length;
+    let doneTasks = tasksCounter;
+    const tasks = taskList.querySelectorAll('li.list-group-item');
+
+    Array.from(tasks).reverse().forEach(task => {
+
+        const doneButton = task.querySelector('button[data-action="done"].active');
+
+        if (doneButton) {
+            task.remove();
+        }
+
+        doneTasks-=1;
+    })
+
+    if (doneTasks > 5) {
+        return
+    }
+
+    for (var i = 0; i <= (5 - doneTasks); i++) {
+        linesContainer.children[i].classList.remove('none');
+    }
+
+    if (doneTasks === 1) {
+        emptyList.classList.remove('none');
+    }
+    console.log(doneTasks);
+}
